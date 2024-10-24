de8e27642c32212c29b949ac1609274c9c83d837
Aysel Bilmez <aybilmez@gmail.com>
2024-10-24 16:22:44 +0300

    Proje dosyası ekle.

---
+++ W4_Inheritance/BaseKisi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4_Inheritance
+{
+    public class BaseKisi //ilk once base class tanimlanir
+    {
+        public string Ad { get; set; } //Ad ve soyad alinir 
+        public string Soyad { get; set; }
+        
+        public virtual void Yazdir() //base metodumuz, ogretmen icin override ornegi yapmak istedigimden virtual olarak belirledim
+        {
+            Console.WriteLine(Ad + " " + Soyad);
+        }
+    }
+
+    public class Ogretmen : BaseKisi //Ogretmen BaseKisi sinifindan metot ve nesneleri kullanabilir
+    {
+        public int MaasBilgisi { get; set; } //ek olarak maas bilgisi girilir
+        public override void Yazdir()  //Burada override kullanmak istedim bir ornegi olmasi icin. Yazdir metodunun icini degistirdim 
+        {
+            Console.WriteLine(Ad + " " + Soyad + " Maas: " + MaasBilgisi + "TL"); 
+        }
+    }
+
+    public class Ogrenci : BaseKisi //yine basekisiden dogan ogrenci sinifi
+    {
+        public int OgrenciNumarasi { get; set; }
+        public void OgrenciBilgileriniYazdir () //burada yeni bir metot tanimlayarak Basekisideki metodu da icinde cagirdim ve ekleme yaptim
+        {
+            Yazdir();
+            Console.WriteLine("No: " + OgrenciNumarasi);
+        }
+    }
+}
---
+++ W4_Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using W4_Inheritance;
+
+Ogretmen ogretmen = new Ogretmen();
+ogretmen.Ad = "Alev";
+ogretmen.Soyad = "Bayindirli";
+ogretmen.MaasBilgisi = 60000;
+
+ogretmen.Yazdir();  //burada override yaptigim icin basekisideki gibi cagirabilirim metodu
+
+Ogrenci ogrenci = new Ogrenci();
+ogrenci.Ad = "Aysel";
+ogrenci.Soyad = "Bilmez";
+ogrenci.OgrenciNumarasi = 2239126;
+
+ogrenci.OgrenciBilgileriniYazdir(); //burada yeni bir metot olusturdugumuz icin yazdir() kullanamayiz tek basina numara gozukmez
+
+Console.ReadKey();
